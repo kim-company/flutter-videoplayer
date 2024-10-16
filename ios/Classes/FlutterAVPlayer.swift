c48ae986ef053e9b0f432aa3edb5ed6701a8f499
Lukas Kompatscher <80333836+LukasKompatscher@users.noreply.github.com>
2024-10-16 18:51:58 +0200

    implemented picture in picture

--- ios/Classes/FlutterAVPlayer.swift
+++ ios/Classes/FlutterAVPlayer.swift
@@ -1,45 +1,72 @@
-//
-//  FlutterAVPlayer.swift
-//  flutter_to_airplay
-//
-//  Created by Junaid Rehmat on 22/08/2020.
-//
-
-import Foundation
 import AVKit
-import MediaPlayer
 import Flutter
+import Foundation
+import MediaPlayer
 
-class FlutterAVPlayer: NSObject, FlutterPlatformView {
-    private var _flutterAVPlayerViewController : AVPlayerViewController;
-    
-    init(frame:CGRect,
-          viewIdentifier: CLongLong,
-          arguments: Dictionary<String, Any>,
-          binaryMessenger: FlutterBinaryMessenger) {
+class FlutterAVPlayer: NSObject, FlutterPlatformView,
+    AVPictureInPictureControllerDelegate
+{
+    private var _flutterAVPlayerViewController: AVPlayerViewController
+    var pipController: AVPictureInPictureController!
+    var pipPossibleObservation: NSKeyValueObservation?
+    var pictureInPictureController: AVPictureInPictureController!
+    var playerLayer: AVPlayerLayer!
+
+    init(
+        frame: CGRect,
+        viewIdentifier: CLongLong,
+        arguments: [String: Any],
+        binaryMessenger: FlutterBinaryMessenger
+    ) {
         _flutterAVPlayerViewController = AVPlayerViewController()
         _flutterAVPlayerViewController.viewDidLoad()
+
+        let player = AVPlayer()
+        _flutterAVPlayerViewController.player = player
+
         if let urlString = arguments["url"] {
             let item = AVPlayerItem(url: URL(string: urlString as! String)!)
-            _flutterAVPlayerViewController.player = AVPlayer(playerItem: item)
+            player.replaceCurrentItem(with: item)
         } else if let filePath = arguments["file"] {
-            let appDelegate = UIApplication.shared.delegate as! FlutterAppDelegate
-            let vc = appDelegate.window.rootViewController as! FlutterViewController
+            let appDelegate =
+                UIApplication.shared.delegate as! FlutterAppDelegate
+            let vc =
+                appDelegate.window.rootViewController as! FlutterViewController
             let lookUpKey = vc.lookupKey(forAsset: filePath as! String)
-            if let path = Bundle.main.path(forResource: lookUpKey, ofType: nil) {
+            if let path = Bundle.main.path(forResource: lookUpKey, ofType: nil)
+            {
                 let item = AVPlayerItem(url: URL(fileURLWithPath: path))
-                _flutterAVPlayerViewController.player = AVPlayer(playerItem: item)
-            }
-    else { // Fixed Issue#29
-                let item = AVPlayerItem(url: URL(fileURLWithPath: filePath as! String))
-                _flutterAVPlayerViewController.player = AVPlayer(playerItem: item)
+                player.replaceCurrentItem(with: item)
+            } else {
+                let item = AVPlayerItem(
+                    url: URL(fileURLWithPath: filePath as! String))
+                player.replaceCurrentItem(with: item)
             }
         }
-        _flutterAVPlayerViewController.player!.play()
-    }
-    func view() -> UIView {
-        return _flutterAVPlayerViewController.view;
-    }
-    
-}
 
+        do {
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(.playback)
+            try audioSession.setMode(.moviePlayback)
+            try audioSession.setActive(true)
+        } catch let e {
+            print(e.localizedDescription)
+        }
+
+        playerLayer = AVPlayerLayer(player: player)
+
+        pictureInPictureController = AVPictureInPictureController(
+            playerLayer: playerLayer)
+
+        if #available(iOS 14.2, *) {
+            pictureInPictureController
+                .canStartPictureInPictureAutomaticallyFromInline = true
+        }
+
+        player.play()
+    }
+
+    func view() -> UIView {
+        return _flutterAVPlayerViewController.view
+    }
+}
